71bcc4c3849bb784c8ded029f2feb0cbfff1ea1a
Pedro Antonio Cano Acosta <cano.iori1@gmail.com>
2021-03-31 16:43:35 -0600

    Actualización de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,26 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
-
+  {
+    path: 'inside-register',
+    loadChildren: () => import('./pages/inside-register/inside-register.module').then( m => m.InsideRegisterPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'outside-register',
+    loadChildren: () => import('./pages/outside-register/outside-register.module').then( m => m.OutsideRegisterPageModule)
+  },
+  {
+    path: 'panel-register',
+    loadChildren: () => import('./pages/panel-register/panel-register.module').then( m => m.PanelRegisterPageModule)
+  },
+  {
+    path: 'truck-register',
+    loadChildren: () => import('./pages/truck-register/truck-register.module').then( m => m.TruckRegisterPageModule)
+  }
 ];
 
 @NgModule({
